b8c33c10be6447aa36a3379f0446cb7f60cd71c0
zhuangyong230-gif <zhuangyong230@gmail.com>
2026-08-14 11:48:29 +0800

    Update Supabase configuration in sync-config.js

--- sync-config.js
+++ sync-config.js
@@ -1,5 +1,5 @@
 window.COMBAT_CLOUD_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://lgwgggxsujdgaxmlvwlz.supabase.co",
+  supabaseAnonKey: "sb_publishable_ukFpCOiSE9lkZQVZsqq3kg_HpUNalv4",
   tableName: "combat_profiles"
 };
